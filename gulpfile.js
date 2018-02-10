097d33734cb48ec316a7220f2e9a2fcac0476b07
System Administrator <root@Ridges-MacBook-Pro.local>
2018-02-09 23:24:56 -0500

    Added Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var coffee = require('gulp-coffee');
-var concat = require('gulp-concat');
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat'),
+    browserify = require('gulp-browserify'),
+    mustache = require('mustache');
 
 var coffeeSources = ['components/coffee/tagline.coffee']
 var jsSources = [
     'components/scripts/rclick.js',
     'components/scripts/pixgrid.js',
     'components/scripts/tagline.js',
-    'components/scripts/template.js',
+    'components/scripts/template.js'
 ];
 gulp.task('coffee', function () {
     gulp.src(coffeeSources)
@@ -21,5 +23,6 @@ gulp.task('coffee', function () {
 gulp.task('js', function () {
     gulp.src(jsSources)
         .pipe(concat('script.js')) // Name given to the file to concatenate
+        .pipe(browserify())
         .pipe(gulp.dest('builds/development/js')); //Destination of file "script.js file that I created above"
 });
