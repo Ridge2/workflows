4397fc7a98695ce7ab3641d0ded1254ac48ff3ba
System Administrator <root@Ridges-MacBook-Pro.local>
2018-02-13 20:53:36 -0500

    Added Environment Variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,6 @@
+// Name: Ridge Francis
+// Date: 2017/02/13
+//
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
@@ -6,60 +9,97 @@ var gulp = require('gulp'),
     browserify = require('gulp-browserify'),
     connect = require('gulp-connect');
 
-var coffeeSources = ['components/coffee/tagline.coffee']
-var jsSources = [
+var outputDir,
+    coffeeSources,
+    jsonSources,
+    jsSources,
+    env,
+    sassSources,
+    sassStyle,
+    htmlSources;
+
+env = process.env.NODE_ENV || 'development';
+
+if (env === 'developement') {
+    outputDir = 'builds/developnent/';
+    sassStyle = 'expanded';
+}
+else {
+    outputDir = 'builds/production/';
+    sassStyle = 'compressed';
+}
+
+coffeeSources = ['components/coffee/tagline.coffee']
+jsSources = [
     'components/scripts/rclick.js',
     'components/scripts/pixgrid.js',
     'components/scripts/tagline.js',
     'components/scripts/template.js'
 ];
-
-var sassSources = ['components/sass/style.scss'];
-
+// Variables for files
+//
+sassSources = ['components/sass/style.scss'];
+htmlSources = [outputDir + '*.html'];
+jsonSources = [outputDir + 'js/data.json'];
 gulp.task('coffee', function () {
     gulp.src(coffeeSources)
         .pipe(coffee({ bare: true })
             .on('error', gutil.log))
         .pipe(gulp.dest('components/scripts'))
 });
-
-
+// compress the javascript sources
+//
 gulp.task('js', function () {
     gulp.src(jsSources)
         .pipe(concat('script.js')) // Name given to the file to concatenate
-        .pipe(browserify())
-        .pipe(gulp.dest('builds/development/js')) //Destination of file "script.js file that I created above"
+        .pipe(browserify()) // js code uses browserify  
+        .pipe(gulp.dest(outputDir + 'js')) //Destination of file "script.js" file that I created above
         .pipe(connect.reload())
 });
-
+// use compass on the image files
+//
 gulp.task('compass', function () {
     gulp.src(sassSources)
         .pipe(compass({
             sass: 'components/sass',
-            image: 'builds/development/images',
-            style: 'expanded'
+            image: outputDir + 'images',
+            style: sassStyle
         }))
         .on('error', gutil.log)
-        .pipe(gulp.dest('builds/development/css'))
+        .pipe(gulp.dest(outputDir + 'css'))
         .pipe(connect.reload())
 });
-
-// Gulp watch waits for changes to files and run them automatically
+// Gulp watch waits for changes to files and reload them automatically
 //
 gulp.task('watch', function () {
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch(jsSources, ['js']);
     gulp.watch('components/sass/*.scss', ['compass']);
+    gulp.watch(htmlSources, ['html']);
+    gulp.watch(jsonSources, ['json']);
 });
 // This code connects you to the server and reloads when there are any changes
 //
 gulp.task('connect', function () {
     connect.server({
-        root: 'builds/development/',
+        root: outputDir,
         livereload: true
     });
+});
+// Reload html
+//
+gulp.task('html', function () {
+    gulp.src(htmlSources)
+        .pipe(connect.reload())
+
+});
+// Reload json 
+//
+gulp.task('json', function () {
+    gulp.src(jsonSources)
+        .pipe(connect.reload())
 });
 
 // default runs this object by default by just running "gulp" in the command line
 //
-gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
